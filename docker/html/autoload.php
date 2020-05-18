a28c4f3969f450ae90eeb19ef5ca886abf874c13
daniel.oliveira <danielso2007@gmail.com>
2020-05-17 23:39:15 -0300

    feat: Orientação a Objetos com PHP: Classes, métodos e atributos.

--- docker/html/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-spl_autoload_register(function ($classes): void {
-    $caminho = '';
-    $dir_class = str_replace("\\", DIRECTORY_SEPARATOR, $classes);
-    // var_dump(getcwd() . $caminho . DIRECTORY_SEPARATOR . "{$dir_class}.php");
-    @include_once getcwd() . $caminho . DIRECTORY_SEPARATOR . "{$dir_class}.php";
-});
